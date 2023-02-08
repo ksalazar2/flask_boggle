a5dafc701a75858375b36568fd77a599cee9c506
Kurt Salazar <ksalazar@kurts-air.home>
2023-02-08 16:18:00 -0500

    complete step 2

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'plswork'
 
 boggle_game = Boggle()
+
+@app.route('/')
+def display_board():
+    """Display game board."""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template('base.html', game_board = board)
