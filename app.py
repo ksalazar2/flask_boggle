4b22d09ca150b9a3ddd887173804643762498067
Kurt Salazar <ksalazar@kurts-air.home>
2023-02-08 15:07:21 -0500

    step 1 - import flask functions

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from boggle import Boggle
+from flask import Flask, request, render_template, redirect, flash, session
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'plswork'
 
 boggle_game = Boggle()
